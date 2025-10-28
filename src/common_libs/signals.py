b758edb4383e3f3d855e43988575d8b30a18321c
Yugo Kato <yugokato.o@gmail.com>
2025-10-28 13:31:57 -0700

    Update exit handler to chain the original handler

--- src/common_libs/signals.py
+++ src/common_libs/signals.py
@@ -9,9 +9,15 @@ def register_exit_handler(func: Callable, *func_args, **func_kwargs):
     """Register an exit handler to make sure the function is always called when a program terminates"""
 
     def wrap_signal_handler(f):
+        orig_handler = signal.getsignal(signal.SIGTERM)
+
         @wraps(f)
         def wrapper(signum, frame):
-            return f(*func_args, **func_kwargs)
+            try:
+                f(*func_args, **func_kwargs)
+            finally:
+                if callable(orig_handler):
+                    orig_handler(signum, frame)
 
         return wrapper
 
