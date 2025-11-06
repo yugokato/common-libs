31555a8de7129a8222be7df0b3559e0114690af6
Yugo Kato <yugokato.o@gmail.com>
2025-11-05 22:09:54 -0800

    Add 'exclude' option to find_open_port()

--- src/common_libs/network.py
+++ src/common_libs/network.py
@@ -1,4 +1,5 @@
 import socket
+from collections.abc import Sequence
 
 from .lock import Lock
 
@@ -18,15 +19,22 @@ def is_port_in_use(port: int, host: str = "127.0.0.1") -> bool:
             return False
 
 
-def find_open_port(start_port: int = 1024, end_port: int = 65535, host: str = "127.0.0.1") -> int:
+def find_open_port(
+    start_port: int = 1024, end_port: int = 65535, host: str = "127.0.0.1", exclude: Sequence[int] | None = None
+) -> int:
     """Find an open port
 
     :param start_port: The fist port number range to check
-    :param start_port: The last port number range to check
+    :param end_port: The last port number range to check
     :param host: The host address to check
+    :param exclude: Ports to exclude from checking
     """
+    if exclude is None:
+        exclude = []
     with Lock("find_open_port"):
         for port in range(start_port, end_port + 1):
+            if port in exclude:
+                continue
             if not is_port_in_use(port, host=host):
                 return port
         raise RuntimeError("Unable to find an open TCP port")
