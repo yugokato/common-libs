a4baa9c5ac6ac168dbea65e8ee5b901769a73f94
Yugo Kato <yugokato.o@gmail.com>
2026-07-31 07:16:23 -0700

    Update the style of failed request logs

--- src/common_libs/clients/rest_client/hooks.py
+++ src/common_libs/clients/rest_client/hooks.py
@@ -127,83 +127,69 @@ def _print_api_summary(response: Response, quiet: bool, rest_client: ClientType,
     if not logger.isEnabledFor(logging.INFO):
         return
 
-    log_headers = rest_client.log_headers
+    log_headers = rest_client.log_headers and not quiet
     request: Request = response.request
-    if quiet:
-        if not response.is_success:
-            # Print to the console regardless of the "quiet" value
-            err = (
-                f"request_id: {request.request_id}\n"
-                f"request: {request.method} {request.url}\n"
-                f"status_code: {response.status_code}\n"
-                f"response: {processed_resp}\n"
-            )
-            sys.stdout.write(color(err, color_code=ColorCodes.RED))
-            sys.stdout.flush()
-    else:
-        bullet = "-"
-        summary = ""
+    bullet = "-"
+    summary = ""
 
-        # request_id
-        summary += color(f"{bullet} request_id: {request.request_id}\n", color_code=ColorCodes.CYAN)
+    # request_id
+    summary += color(f"{bullet} request_id: {request.request_id}\n", color_code=ColorCodes.CYAN)
 
-        # method and url
-        summary += color(f"{bullet} request: {request.method} {request.url}\n", color_code=ColorCodes.CYAN)
+    # method and url
+    summary += color(f"{bullet} request: {request.method} {request.url}\n", color_code=ColorCodes.CYAN)
 
-        # request headers
-        if log_headers:
-            summary += color(
-                f"{bullet} request_headers: {mask_sensitive_headers(dict(request.headers))}\n",
-                color_code=ColorCodes.CYAN,
-            )
-
-        # request payload and query parameters
-        if query_strings := parse_query_strings(str(request.url)):
-            summary += color(f"{bullet} query params: {query_strings}\n", color_code=ColorCodes.CYAN)
-        request_body = process_request_body(request, truncate_bytes=True)
-        if request_body:
-            payload: str
-            try:
-                payload = json.dumps(request_body)
-            except TypeError:
-                if isinstance(request_body, bytes):
-                    payload = request_body.decode("utf-8", errors="replace")
-                else:
-                    payload = str(request_body)
-            payload = str(truncate_body(payload))
-            summary += color(f"{bullet} payload: {payload}\n", color_code=ColorCodes.CYAN)
-
-        # status_code and reason
-        status_color_code = ColorCodes.GREEN if response.is_success else ColorCodes.RED
-        summary += color(f"{bullet} status_code: ", color_code=ColorCodes.CYAN) + color(
-            response.status_code, color_code=status_color_code
+    # request headers
+    if log_headers:
+        summary += color(
+            f"{bullet} request_headers: {mask_sensitive_headers(dict(request.headers))}\n",
+            color_code=ColorCodes.CYAN,
         )
-        if reason := get_response_reason(response):
-            summary += f" ({reason})"
-        summary += "\n"
 
-        # response
-        if not response.is_success:
-            processed_resp = color(processed_resp, color_code=ColorCodes.RED)
-        if processed_resp is not None:
-            summary += color(f"{bullet} response: ", color_code=ColorCodes.CYAN)
-            summary += f"{processed_resp}\n"
+    # request payload and query parameters
+    if query_strings := parse_query_strings(str(request.url)):
+        summary += color(f"{bullet} query params: {query_strings}\n", color_code=ColorCodes.CYAN)
+    request_body = process_request_body(request, truncate_bytes=True)
+    if request_body:
+        payload: str
+        try:
+            payload = json.dumps(request_body)
+        except TypeError:
+            if isinstance(request_body, bytes):
+                payload = request_body.decode("utf-8", errors="replace")
+            else:
+                payload = str(request_body)
+        payload = str(truncate_body(payload))
+        summary += color(f"{bullet} payload: {payload}\n", color_code=ColorCodes.CYAN)
 
-        # response headers
-        if log_headers:
-            summary += color(
-                f"{bullet} response_headers: {mask_sensitive_headers(dict(response.headers))}\n",
-                color_code=ColorCodes.CYAN,
-            )
+    # status_code and reason
+    status_color_code = ColorCodes.GREEN if response.is_success else ColorCodes.RED
+    summary += color(f"{bullet} status_code: ", color_code=ColorCodes.CYAN) + color(
+        response.status_code, color_code=status_color_code
+    )
+    if reason := get_response_reason(response):
+        summary += f" ({reason})"
+    summary += "\n"
 
-        # response time
-        if not response.is_stream:
-            summary += color(
-                f"{bullet} response_time: {response.elapsed.total_seconds()}s\n", color_code=ColorCodes.CYAN
-            )
+    # response
+    if not response.is_success:
+        processed_resp = color(processed_resp, color_code=ColorCodes.RED)
+    if processed_resp is not None:
+        summary += color(f"{bullet} response: ", color_code=ColorCodes.CYAN)
+        summary += f"{processed_resp}\n"
 
-        sys.stdout.write(summary)
-        sys.stdout.flush()
+    # response headers
+    if log_headers:
+        summary += color(
+            f"{bullet} response_headers: {mask_sensitive_headers(dict(response.headers))}\n",
+            color_code=ColorCodes.CYAN,
+        )
+
+    # response time
+    if not quiet and not response.is_stream:
+        summary += color(f"{bullet} response_time: {response.elapsed.total_seconds()}s\n", color_code=ColorCodes.CYAN)
+
+    sys.stdout.write(summary)
+    sys.stdout.flush()
 
 
 def _hook_factory(
